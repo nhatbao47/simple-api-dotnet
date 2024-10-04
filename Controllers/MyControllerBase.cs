5fc086d271e4f839a9be0d42804a9e85449c9835
Dung Nguyen <nhatbao@outlook.com>
2024-10-04 11:10:52 +0700

    Authorize

--- Controllers/MyControllerBase.cs
+++ Controllers/MyControllerBase.cs
@@ -1,10 +1,12 @@
 using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace SimpleApi.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
+[Authorize]
 public class MyControllerBase(SimpleApiContext context, IMapper mapper) : ControllerBase
 {
     protected readonly SimpleApiContext _context = context;
